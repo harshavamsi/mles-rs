a728ce9c4b21b0dbd5ec75d5c233665b1366d374
Jq-rs <jq-rs@mles.io>
2019-06-18 19:26:55 +0000

    Temp save.

--- mles-client/src/ws.rs
+++ mles-client/src/ws.rs
@@ -13,11 +13,13 @@ use std::borrow::Cow;
 use std::collections::HashMap;
 use std::rc::Rc;
 use std::cell::RefCell;
-use std::mem;
+use std::iter;
 
-use futures::stream::Stream;
+use futures::stream::{self, Stream};
 use futures::sync::mpsc::unbounded;
 use futures::{Future, Sink};
+use tokio::io;
+use tokio::io::AsyncRead;
 use tokio::net::TcpListener;
 use tokio::runtime::current_thread::{Runtime, TaskExecutor};
 use tungstenite::protocol::Message;
@@ -27,12 +29,13 @@ use tokio_tungstenite::accept_hdr_async;
 use crate::KEEPALIVE;
 use tokio::net::TcpStream;
 use tokio_codec::Decoder;
-use crate::Bytes;
+use crate::{Bytes, BytesMut};
 use mles_utils::*;
 
 const WSPORT: &str = ":8076";
 const SECPROT: &str = "Sec-WebSocket-Protocol";
 const SECVALUE: &str = "mles-websocket";
+static mut TCPENABLED: u32 = 0;
 
 pub fn process_ws_proxy(raddr: SocketAddr, keyval: String, keyaddr: String) {
     let addr = "0.0.0.0".to_string() + WSPORT;
@@ -83,8 +86,8 @@ pub fn process_ws_proxy(raddr: SocketAddr, keyval: String, keyaddr: String) {
             }
             Err(tungstenite::Error::Protocol(Cow::from(SECPROT)))
         };
-        //let mles_db_hash: HashMap<String, MlesDb> = HashMap::new();
-        //let mles_db = Rc::new(RefCell::new(mles_db_hash));
+        //let ws_db_hash: HashMap<u64, Vec<(u64, u32)> = HashMap::new();
+        //let ws_db = Rc::new(RefCell::new(ws_db_hash));
 
         let accept = accept_hdr_async(stream, callback).map_err(|err|{
             println!("Accept error: {}", err);
@@ -101,103 +104,97 @@ pub fn process_ws_proxy(raddr: SocketAddr, keyval: String, keyaddr: String) {
             let ws_reader = stream.for_each(move |message: Message| {
                 let mles_message = message.into_data();
 
+                //let ws_db_inner = ws_db.clone();
                 let keyval = keyval_inner.clone();
                 let keyaddr = keyaddr_inner.clone();
                 let ws_tx_inner = ws_tx.clone();
                 let (mles_tx, mles_rx) = unbounded();
 
-                if None == key {
-                    println!("Key not found, connecting...");
-                    let tcp = TcpStream::connect(&raddr);
-                    println!("Connected.");
+                println!("Key not found, connecting...{}", cnt);
+                let tcp = TcpStream::connect(&raddr);
+                println!("Connected.");
 
-                    // Check if mles_message matches to local db
-                    let client = tcp.and_then(move |stream| {
-                        let mut keys = Vec::new();
-                        let _val = stream.set_nodelay(true)
-                            .map_err(|_| panic!("Cannot set to no delay"));
-                        let _val = stream.set_keepalive(Some(Duration::new(KEEPALIVE, 0)))
-                            .map_err(|_| panic!("Cannot set keepalive"));
-                        let laddr = match stream.local_addr() {
-                            Ok(laddr) => laddr,
-                            Err(_) => {
-                                let addr = "0.0.0.0:0";
-                                addr.parse::<SocketAddr>().unwrap()
-                            }
-                        };
-                        if  !keyval.is_empty() {
-                            keys.push(keyval);
-                        } else {
-                            keys.push(MsgHdr::addr2str(&laddr));
-                            if !keyaddr.is_empty() {
-                                keys.push(keyaddr);
-                            }
+                // Check if mles_message matches to local db
+                let client = tcp.and_then(move |stream| {
+                    let mut keys = Vec::new();
+                    let _val = stream.set_nodelay(true)
+                        .map_err(|_| panic!("Cannot set to no delay"));
+                    let _val = stream.set_keepalive(Some(Duration::new(KEEPALIVE, 0)))
+                        .map_err(|_| panic!("Cannot set keepalive"));
+                    let laddr = match stream.local_addr() {
+                        Ok(laddr) => laddr,
+                        Err(_) => {
+                            let addr = "0.0.0.0:0";
+                            addr.parse::<SocketAddr>().unwrap()
+                        }
+                    };
+                    if  !keyval.is_empty() {
+                        keys.push(keyval);
+                    } else {
+                        keys.push(MsgHdr::addr2str(&laddr));
+                        if !keyaddr.is_empty() {
+                            keys.push(keyaddr);
+                        }
+                    }
+                    //let mut mles_db_once = ws_db_inner.borrow_mut();
+                    //mles_db_once.insert(key.unwrap().clone(), keys.clone());
+
+                    //let (mut sink, stream) = Bytes.framed(stream).split();
+                    let (reader, writer) = stream.split();
+
+                    let socket_writer = mles_rx.fold(writer, move |writer, buf: Vec<u8>| {
+                        if None == key {
+                            //create hash for verification
+                            let decoded_message = Msg::decode(buf.as_slice());
+                            keys.push(decoded_message.get_uid().to_string());
+                            keys.push(decoded_message.get_channel().to_string());
+                            key = Some(MsgHdr::do_hash(&keys));
+                            cid = Some(MsgHdr::select_cid(key.unwrap()));
                         }
 
-                        let (mut sink, stream) = Bytes.framed(stream).split();
+                        if buf.is_empty() {
+                            println!("Empty buffer");
+                            return Err(Error::new(ErrorKind::BrokenPipe, "broken pipe"));
+                        }
+                        let msghdr = MsgHdr::new(buf.len() as u32, cid.unwrap(), key.unwrap());
+                        let mut msgv = msghdr.encode();
+                        msgv.extend(buf);
 
-                        let mles_rx = mles_rx.map_err(|_| panic!()); // errors not possible on rx XXX
+                        //send message forward
+                        let amt = io::write_all(writer, msgv);
+                        let amt = amt.map(|(writer, _)| writer);
+                        amt.map_err(|_| ())
+                    });
 
-                        let mles_rx = mles_rx.for_each(move |buf: Vec<_>| {
-                            if None == key {
-                                //create hash for verification
-                                let decoded_message = Msg::decode(buf.as_slice());
-                                keys.push(decoded_message.get_uid().to_string());
-                                keys.push(decoded_message.get_channel().to_string());
-                                key = Some(MsgHdr::do_hash(&keys));
-                                cid = Some(MsgHdr::select_cid(key.unwrap()));
-                            }
-
-                            if buf.is_empty() {
-                                println!("Empty buffer");
-                                return Err(Error::new(ErrorKind::BrokenPipe, "broken pipe"));
-                            }
-                            let msghdr = MsgHdr::new(buf.len() as u32, cid.unwrap(), key.unwrap());
-                            let mut msgv = msghdr.encode();
-                            msgv.extend(buf);
-                            let _ = sink.start_send(msgv).map_err(|err| {
-                                Error::new(ErrorKind::Other, err)
-                            });
-                            let _ = sink.poll_complete().map_err(|err| {
-                                Error::new(ErrorKind::Other, err)
-                            });
-                            Ok(())
+                    let iter = stream::iter_ok::<_, io::Error>(iter::repeat(()));
+                    let socket_reader = iter.fold(reader, move |reader, _| {
+                        let buf = io::read_exact(reader, BytesMut::from(vec![0;32]));
+                        println!("Got buffer!");
+                        let _ = ws_tx.send(buf.to_vec()).wait().map_err(|err| {
+                            println!("Send error");
+                            Error::new(ErrorKind::Other, err)
                         });
 
-                        //let send_wsrx = mles_rx.forward(sink);
-                        let write_wstx = stream.for_each(move |buf| {
-                            let ws_tx = ws_tx_inner.clone();
-                            // send to websocket
-                            let _ = ws_tx.send(buf.to_vec()).wait().map_err(|err| {
-                                println!("Send error");
-                                Error::new(ErrorKind::Other, err)
-                            });
-                            Ok(())
-                        });
+                        reader
+                    });
+                    let socket_reader = socket_reader.map_err(|_| {});
+                    let conn = socket_reader.map(|_| ()).select(socket_writer.map(|_| ()));
 
-                        let send_wsrx = mles_rx.map(|_| {}).map_err(|_| {});
-                        TaskExecutor::current().spawn_local(Box::new(send_wsrx.then(move |_| {
-                            println!("Connection rx closed.");
-                            Ok(())
-                        }))).unwrap();
-                        
-                        let write_wstx = write_wstx.map(|_| ()).map_err(|_| {});
-                        TaskExecutor::current().spawn_local(Box::new(write_wstx.then(move |_| {
-                            println!("Connection tx closed.");
-                            Ok(())
-                        }))).unwrap();
-                        Ok(())
-                    }).map_err(|_| {});
-                    TaskExecutor::current().spawn_local(Box::new(client.then(move |_| {
+                    TaskExecutor::current().spawn_local(Box::new(conn.then(move |_| {
                         println!("Connection {} proxy closed.", cnt);
                         Ok(())
                     }))).unwrap();
-                }
 
-                let _ = mles_tx.send(mles_message.clone()).wait().map_err(|err| {
-                    println!("Error {}", err);
-                    Error::new(ErrorKind::Other, err)
-                });
+                    let _ = mles_tx.send(mles_message.clone()).wait().map_err(|err| {
+                        println!("Error {}", err);
+                        Error::new(ErrorKind::Other, err)
+                    });
+                    Ok(())
+                }).map_err(|_| {});
+                TaskExecutor::current().spawn_local(Box::new(client.then(move |_| {
+                    println!("Connection {} client closed.", cnt);
+                    Ok(())
+                }))).unwrap();
                 Ok(())
             });
 
